2a8b68a75c2e279a71952fb417666029726f6f35
Cody <codybarker.or@gmail.com>
2023-05-22 14:32:50 -0700

    working on routes to individual farms

--- src/FarmDetail.js
+++ src/FarmDetail.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-function FarmDetail() {
+function FarmDetail({farm}) {
+
+    
     return(
         <div>
-            Farm Detail
+            {farm.name}
         </div>
     )
 }
