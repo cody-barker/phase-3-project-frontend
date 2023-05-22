6364f5c45b709d289d613bd988a1f297a98d320d
Cody <codybarker.or@gmail.com>
2023-05-22 14:10:05 -0700

    initial app, index, and home setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import NavBar from './NavBar'
 import './App.css';
 
 function App() {
+   
+  const [allFarms, setAllFarms] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/farms")
+    .then(r => r.json())
+    .then(farms => setAllFarms(farms))
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+        <Routes>
+          <Route 
+            path="/" 
+            element={<Home allFarms={allFarms} setAllFarms={setAllFarms}/>}/>
+        </Routes>
     </div>
   );
 }
